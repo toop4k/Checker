7a7b87417a747d8019eaae5113efaf0d0d60bfe9
tupac <tupackapustiak@gmail.com>
2023-12-15 11:44:42 -0400

    prueba de lista

---
+++ src/app/shared/mock-users.ts
@@ -0,0 +1,10 @@
+import { User } from './User';
+
+//Hardcodeamos un par de users
+export const Users: User[] = [
+  new User('1', 'Usuario1', 'Subtítulo1', 'url_imagen1', 100),
+  new User('2', 'Usuario2', 'Subtítulo2', 'url_imagen2', 150),
+  new User('3', 'Usuario3', 'Subtítulo3', 'url_imagen3', 200),
+  new User('4', 'Usuario4', 'Subtítulo4', 'url_imagen4', 120),
+  new User('5', 'Usuario5', 'Subtítulo5', 'url_imagen5', 80),
+];
