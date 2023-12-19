09b1518c2d315af6448354853a38ac9c2c397156
tupac <tupackapustiak@gmail.com>
2023-12-19 15:13:12 -0400

    Mejorando Login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,3 +1,5 @@
+// login.component.ts
+
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { Us, US } from '../interface/us';
@@ -8,29 +10,28 @@ import { encrypt } from '../util/util-encrypt';
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-
 })
 export class LoginComponent {
   username: string = '';
   password: string = '';
+  incorrectPassword: boolean = false; // Variable para controlar si la contraseña es incorrecta
 
-  validationUsList:Us[] =US;
+  validationUsList: Us[] = US;
 
   constructor(private router: Router) {}
 
   onSubmit(nameLogin: string, passRequest: string): void {
     // @ts-ignore
-    let usOpt :Us[] = this.validationUsList.filter(
-      Item => Item.us == nameLogin
-    );
-    
+    let usOpt: Us[] = this.validationUsList.filter((Item) => Item.us == nameLogin);
+
     console.log(encrypt(passRequest));
 
-     // @ts-ignore
-    if (usOpt.length>0 && (usOpt.at(0).pass)==encrypt(passRequest)) {
-      
-      this.router.navigate(['/begin',nameLogin]);
+    // @ts-ignore
+    if (usOpt.length > 0 && usOpt[0].pass == encrypt(passRequest)) {
+      this.router.navigate(['/begin', nameLogin]);
     } else {
+      // Contraseña incorrecta, establece la variable incorrectPassword a true
+      this.incorrectPassword = true;
       console.error('Por favor, ingresa un nombre de usuario y una contraseña.');
     }
   }
